f327a67b369f429a09d646b4c33d98f2ead8497e
palam <pawarit.a@deeple.ai>
2021-11-23 01:03:35 +0700

    feat: add two more data cleaning method, there were separeted from tokenize module

--- services/data_cleaning.py
+++ services/data_cleaning.py
@@ -4,6 +4,23 @@ from typing import Dict
 emoji_pattern = re.compile(r"[\u0E00-\u0E7Fa-zA-Z0-9' \n\-!$%^&*()_+|~=`{}\[\]:'<>?,.#\/]")
 
 
+def replace_and_remove_tags(text):
+    text = (
+        text.strip()
+        .replace("FACILITY", "LOCATION")
+        .replace("[AGO]", "")
+        .replace("[/AGO]", "")
+        .replace("[T]", "")
+        .replace("[/T]", "")
+    )
+    text = re.sub("<[^>]*>", "", text)
+    return text
+
+
+def normalize_quote(text):
+    return text.replace("''", '"').replace("’", '"').replace("‘", '"')
+
+
 def normalize_space(text):
     text = text.replace("\t", "")
     text = text.replace("\u200b", "")
