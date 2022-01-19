fad9c3151e1d4f86f49b9d58df7ffc05bbc63727
palam <pawarit.a@deeple.ai>
2022-01-19 22:26:07 +0700

    feat: initial commit of controllers/training.py

---
+++ controllers/training.py
@@ -0,0 +1,31 @@
+from ..services.models import thai2fit_model
+from ..services.models.dataset import char_to_index, encode_character_input, get_dataset, padding_dataset
+from ..services.models.model import create_models, fit_model
+
+
+def train_model_controller(is_padding=False):
+    n_word = len(thai2fit_model.index2word)
+    n_char = len(char_to_index())
+    max_len_word = 284
+    max_len_char = 30
+
+    dataset = get_dataset("dataset/ner.data")
+    x_char_dataset = encode_character_input(dataset["train_word"], max_len_word, max_len_char)
+    y_char_dataset = encode_character_input(dataset["test_word"], max_len_word, max_len_char)
+    ner_label = sorted(set(dataset["train_target"]))
+    if is_padding:
+        dataset = padding_dataset(dataset, max_len_word)
+
+    model = create_models(n_word, n_char, ner_label, max_len_word=max_len_word)
+    history, model = fit_model(
+        model,
+        dataset["train_word"],
+        x_char_dataset,
+        dataset["train_target"],
+        dataset["test_word"],
+        y_char_dataset,
+        dataset["test_target"],
+    )
+
+    save_filepath = "saved_model/last_weight-50.hdf5"
+    model.save_weights(save_filepath)
