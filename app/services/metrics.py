bad4086710b31f90b496b5f263590ecedb0483c5
palam <pawarit.a@deeple.ai>
2022-04-05 15:05:16 +0700

    refactor: move F1Score metric to metrics.py

--- app/services/metrics.py
+++ app/services/metrics.py
@@ -1,8 +1,13 @@
 from itertools import chain
+from typing import Optional
 
 import numpy as np
+import tensorflow as tf
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import LabelBinarizer
+from tensorflow_addons.metrics import FBetaScore
+from tensorflow_addons.utils.types import AcceptableDTypes, FloatTensorLike
+from typeguard import typechecked
 
 
 def convert_model_prediction(y_true, y_pred, ner_label_index):
@@ -38,3 +43,28 @@ def ner_classification_report(y_true, y_pred):
         target_names=tagset,
         digits=4,
     )
+
+
+class F1Score(FBetaScore):
+    @typechecked
+    def __init__(
+        self,
+        num_classes: FloatTensorLike,
+        average: str = None,
+        threshold: Optional[FloatTensorLike] = None,
+        name: str = "f1_score",
+        dtype: AcceptableDTypes = None,
+    ):
+        self.num_classes = num_classes
+        super().__init__(num_classes, average, 1.0, threshold, name=name, dtype=dtype)
+
+    def update_state(self, y_true: tf.Tensor, y_pred: tf.Tensor, sample_weight=None):
+        y_true = tf.one_hot(y_true, self.num_classes)
+        y_pred = tf.one_hot(y_pred, self.num_classes)
+        for idx in tf.range(tf.shape(y_true)[0]):
+            super(F1Score, self).update_state(y_true[idx], y_pred[idx], None)
+
+    def get_config(self):
+        base_config = super().get_config()
+        del base_config["beta"]
+        return base_config
--- app/services/model.py
+++ app/services/model.py
@@ -1,9 +1,5 @@
-from datetime import datetime
 from pathlib import Path
-from typing import Optional
 
-import numpy as np
-import tensorflow as tf
 from gensim.models import KeyedVectors
 from tensorflow.keras import Model, metrics
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
@@ -18,10 +14,9 @@ from tensorflow.keras.layers import (
     TimeDistributed,
     concatenate,
 )
-from tensorflow_addons.metrics import FBetaScore
 from tensorflow_addons.text.crf_wrapper import CRFModelWrapper
-from tensorflow_addons.utils.types import AcceptableDTypes, FloatTensorLike
-from typeguard import typechecked
+
+from .metrics import F1Score
 
 thai2fit_model: KeyedVectors = KeyedVectors.load_word2vec_format("thai2fit/thai2vecNoSym.bin", binary=True)
 
@@ -72,31 +67,6 @@ class CharacterEmbeddingBlock(Layer):
 
     def get_config(self):
         return self._config
-
-
-class F1Score(FBetaScore):
-    @typechecked
-    def __init__(
-        self,
-        num_classes: FloatTensorLike,
-        average: str = None,
-        threshold: Optional[FloatTensorLike] = None,
-        name: str = "f1_score",
-        dtype: AcceptableDTypes = None,
-    ):
-        self.num_classes = num_classes
-        super().__init__(num_classes, average, 1.0, threshold, name=name, dtype=dtype)
-
-    def update_state(self, y_true: tf.Tensor, y_pred: tf.Tensor, sample_weight=None):
-        y_true = tf.one_hot(y_true, self.num_classes)
-        y_pred = tf.one_hot(y_pred, self.num_classes)
-        for idx in tf.range(tf.shape(y_true)[0]):
-            super(F1Score, self).update_state(y_true[idx], y_pred[idx], None)
-
-    def get_config(self):
-        base_config = super().get_config()
-        del base_config["beta"]
-        return base_config
 
 
 def create_models(
